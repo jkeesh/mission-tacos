32d756c25799c2eb2d73d3a097020e162d77ac19
Eric Conner <eric@pinterest.com>
2013-03-31 17:09:31 -0700

    Add redis backend.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ MISSION TACOS
 
 To get started:
 
+Install python requirements:
+
     cd ..
     mkdir mission-tacos-env
     virtualenv mission-tacos-env
@@ -11,5 +13,17 @@ To get started:
     pip install -r requirements.txt
 
     python app/start.py
+
+Install redis:
+
+    curl -O http://redis.googlecode.com/files/redis-2.6.8.tar.gz
+    tar xzf redis-2.6.8.tar.gz
+    cd redis-2.6.8
+    make
+
+Copy over redis init scripts:
+
+    sudo cp src/redis-server /usr/local/bin/
+    sudo cp src/redis-cli /usr/local/bin/
 
 Navigate in yo' web browsing device to http://localhost:8000/
---
+++ app/models/base_model.py
@@ -0,0 +1,67 @@
+import json
+
+from dictshield.document import Document
+from dictshield.fields import IntField
+from dictshield.fields import UUIDField
+from dictshield.fields import StringField
+
+from redis_connection import redis_conn
+
+
+class Model(Document):
+    key_prefix = None
+
+    # every model should have an id
+    obj_id = IntField()
+
+    def save(self):
+        if not self.key_prefix:
+            raise Exception("Key prefix not defined!")
+
+        # if id is not set, assume we're creating object
+        if not self.id:
+            self.obj_id = redis_conn.incr("%s:id" % self.key_prefix)
+
+        redis_conn.set("%s:%s" % (self.key_prefix, self.obj_id), self.to_json())
+        return self.obj_id
+
+    @classmethod
+    def get_one(cls, obj_id):
+        if not cls.key_prefix:
+            raise Exception("Key prefix not defined!")
+
+        if not obj_id:
+            return None
+
+        obj_str = redis_conn.get("%s:%s" % (cls.key_prefix, obj_id))
+        if not obj_str:
+            return None
+
+        obj_json = json.loads(obj_str)
+        return cls(**obj_json)
+
+
+class User(Model):
+
+    key_prefix = "user"
+
+    username = StringField(max_length=1024)
+    password = StringField(max_length=5000)
+
+    @classmethod
+    def get_by_username(cls, username):
+        max_id = redis_conn.get("%s:id" % cls.key_prefix)
+        if not max_id:
+            return None
+        for idx in xrange(1, int(max_id) + 1):
+            user = cls.get_one(idx)
+            if not user:
+                continue
+
+            if user.username == username:
+                return user
+        return None
+
+
+
+
