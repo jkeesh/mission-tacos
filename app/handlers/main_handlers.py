cd240b4738911cf55765a49c3640dc19e872dcf6
Jeremy Keeshin <jkeesh@gmail.com>
2013-03-31 17:28:43 -0700

    remove unused code

--- app/handlers/main_handlers.py
+++ app/handlers/main_handlers.py
@@ -6,11 +6,6 @@ import options
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        # user = User(username="test", password="mypassword")
-        # user.save()
-
-        user = User.get_by_username("test")
-        print user.username
 
         self.render('index.html',
                     debug=options.cli_args.debug)
