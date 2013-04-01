72f0c737ebff4082ad84a16b70b366495820ea80
Jeremy Keeshin <jkeesh@gmail.com>
2013-03-31 17:21:29 -0700

    add date file

---
+++ static/js/data.js
@@ -0,0 +1,44 @@
+TACO_PLACES = [
+	{	
+		"name": "La Taqueria",
+		"addr": "2889 Mission St",
+		"lat": 37.750884,
+		"long": -122.418055
+	},
+	{	
+		"name": "Taqueria Altena",
+		"addr": "2588 Mission Street",
+		"lat": 37.755704,
+		"long": -122.418808
+	}
+	// {	
+	// 	"name": "La Taqueria",
+	// 	"addr": "2889 Mission St",
+	// 	"lat": 37.750884,
+	// 	"long": -122.418055
+	// },
+	// {	
+	// 	"name": "La Taqueria",
+	// 	"addr": "2889 Mission St",
+	// 	"lat": 37.750884,
+	// 	"long": -122.418055
+	// },
+	// {	
+	// 	"name": "La Taqueria",
+	// 	"addr": "2889 Mission St",
+	// 	"lat": 37.750884,
+	// 	"long": -122.418055
+	// },
+	// {	
+	// 	"name": "La Taqueria",
+	// 	"addr": "2889 Mission St",
+	// 	"lat": 37.750884,
+	// 	"long": -122.418055
+	// },
+	// {	
+	// 	"name": "La Taqueria",
+	// 	"addr": "2889 Mission St",
+	// 	"lat": 37.750884,
+	// 	"long": -122.418055
+	// }
+];
