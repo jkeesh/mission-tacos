568e58b47c79b771e023c9e56b1c8e304c9c2a74
Jeremy Keeshin <jkeesh@gmail.com>
2013-04-02 23:22:56 -0700

    lots more

--- static/js/data.js
+++ static/js/data.js
@@ -23,8 +23,8 @@ TACO_PLACES = [
     {
         "addr": "2779 Mission St", 
         "hash": "496d241b9f", 
-        "lat": 37.7525766, 
-        "lng": -122.4184731, 
+        "lat": 37.75257879999999, 
+        "lng": -122.4184733, 
         "name": "Taquerias El Farolito"
     }, 
     {
@@ -61,5 +61,159 @@ TACO_PLACES = [
         "lat": 37.7604856, 
         "lng": -122.4193798, 
         "name": "Taqueria Cancun"
+    }, 
+    {
+        "addr": "699 Valencia St", 
+        "hash": "06a11e6e9a", 
+        "lat": 37.7617363, 
+        "lng": -122.4214412, 
+        "name": "Taqueria El Buen Sabor"
+    }, 
+    {
+        "addr": "2778 24th St", 
+        "hash": "69ed38aa9d", 
+        "lat": 37.753107, 
+        "lng": -122.407806, 
+        "name": "Casa Sanchez"
+    }, 
+    {
+        "addr": "2092 Mission St", 
+        "hash": "fd06b549a1", 
+        "lat": 37.763635, 
+        "lng": -122.4196945, 
+        "name": "Taqueria El Castillito"
+    }, 
+    {
+        "addr": "3036 16th St", 
+        "hash": "614d909bf1", 
+        "lat": 37.7649291, 
+        "lng": -122.4210117, 
+        "name": "Taqueria Los Coyotes"
+    }, 
+    {
+        "addr": "515 Valencia St", 
+        "hash": "aa1e5ab2ef", 
+        "lat": 37.76457000000001, 
+        "lng": -122.421589, 
+        "name": "La Cumbre Taqueria"
+    }, 
+    {
+        "addr": "2399 Folsom St", 
+        "hash": "543892eeb8", 
+        "lat": 37.758928, 
+        "lng": -122.4145089, 
+        "name": "El Faro"
+    }, 
+    {
+        "addr": "23rd St & Treat St", 
+        "hash": "c47793f584", 
+        "lat": 37.75416999999999, 
+        "lng": -122.4131527, 
+        "name": "El Gallo Giro Taco Truck"
+    }, 
+    {
+        "addr": "2471 Mission St", 
+        "hash": "b0eb25859e", 
+        "lat": 37.757548, 
+        "lng": -122.418593, 
+        "name": "La Pinata"
+    }, 
+    {
+        "addr": "2406 Bryant St", 
+        "hash": "f0fe9c066f", 
+        "lat": 37.7556637, 
+        "lng": -122.4095818, 
+        "name": "El Metate"
+    }, 
+    {
+        "addr": "2560 Marin Street", 
+        "hash": "ba9863da33", 
+        "lat": 37.74853, 
+        "lng": -122.403008, 
+        "name": "El Tonayense"
+    }, 
+    {
+        "addr": "2884 24th St", 
+        "hash": "344897fe03", 
+        "lat": 37.7528426, 
+        "lng": -122.4100064, 
+        "name": "La Palma"
+    }, 
+    {
+        "addr": "3071 16th St", 
+        "hash": "1ff4da34b7", 
+        "lat": 37.764857, 
+        "lng": -122.4210934, 
+        "name": "Pancho Villa Taqueria"
+    }, 
+    {
+        "addr": "3409 24th St", 
+        "hash": "d95eec129a", 
+        "lat": 37.752013, 
+        "lng": -122.4210166, 
+        "name": "Papalote Mexican Grill"
+    }, 
+    {
+        "addr": "2801 Folsom St", 
+        "hash": "2ac549be2e", 
+        "lat": 37.7523615, 
+        "lng": -122.4137365, 
+        "name": "La Parrilla Grill"
+    }, 
+    {
+        "addr": "3146 24th St", 
+        "hash": "2e01ee69fc", 
+        "lat": 37.7524952, 
+        "lng": -122.4151048, 
+        "name": "Taqueria Guadalajara"
+    }, 
+    {
+        "addr": "2794 24th St", 
+        "hash": "c60ca90901", 
+        "lat": 37.7529693, 
+        "lng": -122.4080869, 
+        "name": "Taqueria San Francisco"
+    }, 
+    {
+        "addr": "2830 Mission St", 
+        "hash": "f93b8e04ac", 
+        "lat": 37.7517904, 
+        "lng": -122.4186239, 
+        "name": "Taqueria San Jose"
+    }, 
+    {
+        "addr": "2893 Mission St", 
+        "hash": "65ce26c95f", 
+        "lat": 37.7508392, 
+        "lng": -122.4181962, 
+        "name": "Elsy's Restaurant"
+    }, 
+    {
+        "addr": "3306 Mission St", 
+        "hash": "ac0b0c4864", 
+        "lat": 37.74384879999999, 
+        "lng": -122.4212146, 
+        "name": "El Gran Taco Loco"
+    }, 
+    {
+        "addr": "2198 Folsom St", 
+        "hash": "804f6e2fd2", 
+        "lat": 37.7622168, 
+        "lng": -122.415145, 
+        "name": "El Tepa Taqueria"
+    }, 
+    {
+        "addr": "598 Valencia St", 
+        "hash": "723a5fa1c4", 
+        "lat": 37.7633611, 
+        "lng": -122.4218883, 
+        "name": "El Toro Taqueria"
+    }, 
+    {
+        "addr": "2916 24th St", 
+        "hash": "c86d6eb84d", 
+        "lat": 37.7527704, 
+        "lng": -122.4104463, 
+        "name": "La Espiga De Oro"
     }
 ]
